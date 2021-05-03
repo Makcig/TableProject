5e3514e029b9faa4b2a038369be3ddd3b4cdfde5
Maxim Igolkin <niidel@gmail.com>
2021-05-03 23:12:17 +0300

    Final release

---
+++ customerController.js
@@ -0,0 +1,138 @@
+'use strict'
+
+// Asenna ensin mysql driver 
+// npm install mysql --save
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host : 'localhost',
+  port : 3306, 
+  user : 'root',
+  password : 'Ruutti',
+  database : 'asiakas'
+});
+
+module.exports = 
+{
+    fetchTypes: function (req, res) {  
+      connection.query('SELECT Avain, Lyhenne, Selite FROM Asiakastyyppi', function(error, results, fields){
+        if ( error ){
+          console.log("Virhe haettaessa data asiakas-taulusta: " + error);
+          res.status(500);
+          res.json({"status:" : "ei toimii"});
+        }
+        else
+        {
+          console.log("Data = " + JSON.stringify(results));
+          res.json(results);
+        }
+    });
+
+    },
+    // Tehtävä 2 ja 3
+    fetchCustomers: function(req, res){
+      var nimi = req.query.NIMI;  
+      var osoite = req.query.OSOITE;
+      var asty_avain = req.query.ASTY_AVAIN;
+      var sql = 'SELECT * from Asiakas WHERE 1 = 1';
+      sql = sql + " AND nimi like '" + nimi + "%'" + " AND osoite like '" + osoite + "%'";
+
+      if(asty_avain != null){
+        sql = sql + " AND asty_avain like '" + asty_avain + "%'";
+      }
+
+      connection.query(sql, function(error, results, fields){
+        if ( error ){
+          console.log("Virhe haettaessa data asiakas-taulusta: " + error);
+          res.status(500);
+          res.json({"status:" : "ei toimii"});
+        }
+        else
+        {
+          console.log("Data = " + JSON.stringify(results));
+          res.json(results);
+        }
+      });
+    },
+
+    // Tehtävä 4
+    create: function(req, res){
+      var nimi = req.body.NIMI;  
+      var osoite = req.body.OSOITE;
+      var postinro = req.body.POSTINRO;
+      var postitmp = req.body.POSTITMP;
+      var asty_avain = req.body.ASTY_AVAIN;
+      // Tehtävä 9
+      if((nimi == "" || nimi == null) || (osoite == "" || osoite == null) || (postinro == "" || postinro == null) || (postitmp == "" || postitmp == null)){
+        res.status(400);
+        res.json({"status": "Ei onnistunut", "error" : "Kaikki kentät pitäisi olla täytetty"});
+      }
+      else{
+        var sql = "INSERT INTO asiakas (nimi, osoite, postinro, postitmp, luontipvm, asty_avain)"
+        sql += " VALUES ('" + nimi + "', '" + osoite + "', '" + postinro + "', '" + postitmp + "', curDate(), '" + asty_avain + "')";
+        connection.query(sql, function(error, results, fields){
+          if ( error ){
+            console.log("Virhe lisäämisessä asiakasta tauluun: " + error);
+            res.status(500);
+            res.json({"status:" : "ei toimii"});
+          }
+          else
+          {
+            res.status(201);
+            console.log("Data = " + JSON.stringify(results));
+            res.json("Lisääminen onnistui.");
+          }
+        });
+      }
+    },
+    // Tehtävä 10
+    update: function(req, res){
+      var avain = req.params.id;
+      var sql = "UPDATE asiakas SET nimi='" + req.body.NIMI + "', osoite='" + req.body.OSOITE + "', postinro='" + req.body.POSTINRO + "', postitmp='" + req.body.POSTITMP + "', asty_avain='" + req.body.ASTY_AVAIN + "' WHERE avain=" + avain;
+      connection.query(sql, function(error, results, fields){
+        if(error){
+          console.log("Virhe asiakkaan muokkauksessa: " + error);
+          res.status(500);
+          res.json({"status:" : "ei toimii"});
+        }
+        else{
+          res.json("Muokkaus onnistui");
+          res.status(202);
+        }
+      });
+    },
+
+    // Tehtävä 6
+    delete : function (req, res) {
+      var avain = req.params.id;
+      connection.query("DELETE FROM asiakas WHERE avain=" + avain, function(error, results, fields){
+        if ( error ){
+          console.log("Virhe asiakkaan poistossa: " + error);
+          res.status(500);
+          res.json({"status:" : "ei toimii"});
+        }
+        else
+        {
+          console.log("Data = " + JSON.stringify(results));
+          res.json("Poisto onnistui.");
+        }
+      })
+    },
+    // Muokkausta varten
+    getCustomerById : function(req, res){
+      var avain = req.params.id;
+      connection.query("SELECT * FROM asiakas WHERE avain=" + avain, function(error, results, fields){
+        if ( error ){
+          console.log("Virhe asiakkaan otossa: " + error);
+          res.status(500);
+          res.json({"status:" : "ei toimii"});
+        }
+        else
+        {
+          console.log("Data = " + JSON.stringify(results));
+          res.json(results);
+        }
+      })
+    }
+}
---
+++ js/ajax.js
@@ -0,0 +1,169 @@
+$(document).ready(function () {
+    // haetaan data
+    fetch = (fromform) => {
+        $("#data tr#elements").remove();
+        if(document.getElementById("Tyyppi").value == "all"){ //jos valittu "kaikki" haku tapahtu vaan inputien kautta
+            $.get({
+                url: `http://127.0.0.1:3002/Asiakas?` + $(fromform + ` input`).serialize(),
+                success: (result) => {
+                    showResultInTable(result);
+                },
+                error: function (){
+                    alert($(fromform ` input`).serialize());
+                    alert("1You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1")
+                }
+            });
+        }else{
+            $.get({
+                url: `http://127.0.0.1:3002/Asiakas?` + $(fromform).serialize(),
+                success: (result) => {
+                    showResultInTable(result);
+                },
+                error: function (){
+                    alert("You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1")
+                }
+            });
+        }
+    }
+
+    $('#searchBtn').click(() => {
+        fetch(`form#haku`);
+    });
+
+    // Haetaan asiakastyypit
+    $.get("http://127.0.0.1:3002/Types", function(data){
+        var addOpt = '<option value="all">Kaikki</option>';
+        var addOptLis = '';
+        for(var x of data){
+            addOpt += '<option value=' + x.Avain + '>' + x.Selite + '</option>';
+            addOptLis += '<option value=' + x.Avain + '>' + x.Selite + '</option>';
+        }
+        $("#Tyyppi").html(addOpt);
+        $("#Tyyppi_lis").html(addOptLis);
+        $("#Tyyppi_edit").html(addOptLis);
+    });
+
+
+    $("#lisaaBtn").click(() => {
+        lisaus.dialog( "open" );
+    });
+
+    // Lisääminen (Tehtävä 4)
+    lisaus = $( "#dialog-form" ).dialog({
+        autoOpen: false,    
+        height: 540,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Lisää": function () {
+                $.ajax({
+                    type: "POST",
+                    url: "http://127.0.0.1:3002/Asiakas",
+                    data: $(`form#lisaus`).serialize(),
+                    success: function(data){
+                        alert(data);
+                        fetch(`form#lisaus`);
+                        lisaus.dialog( "close" );
+                    },
+                    error: function(data){
+                        if(data.status == 400){
+                            alert(data.responseText);
+                        }
+                        else{
+                            alert("Jotain epäonnistunut");
+                        }
+                    }
+                })
+            },
+            Cancel: function() {
+                lisaus.dialog( "close" );
+            } 
+        }
+    });
+    // Tehtävä 7
+    poista = (avain) => {
+
+        $.ajax({
+            url: 'http://127.0.0.1:3002/Asiakas/' + avain,
+            type: 'DELETE',
+            contentType: 'application/json',
+            success: (function(result) {
+                alert("Poisto onnistui.");
+                fetch(`form#haku`);
+                console.log(result);       
+            }),
+            error:  (function(ajaxContext){
+                alert(ajaxContext.responseText);
+            })
+            
+        });
+    },
+
+    // Тehtävä 10
+    edit = (avain) =>{
+        $.ajax({
+            url: `http://127.0.0.1:3002/Asiakas/` + avain,
+            type: 'GET',
+            //contentType: 'application/json',
+            success: (result) => {
+                result.forEach(element => {
+                    $("#avain_edit").text(element.AVAIN);
+                    $("#nimi_edit").val(element.NIMI);
+                    $("#osoite_edit").val(element.OSOITE);
+                    $("#postinro_edit").val(element.POSTINRO);
+                    $("#postitmp_edit").val(element.POSTITMP);
+                    $("#Tyyppi_edit").val(element.ASTY_AVAIN);
+                    return;
+                })
+            },
+            error: (function(ajaxContext){
+                console.log("Virhe haettaessa")
+            })
+        });
+        editwindow.data("id", avain).dialog( "open" );
+    },
+    editwindow = $( "#dialog-edit" ).dialog({
+        autoOpen: false,
+        height: 540,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Muokata": function () {
+                $.ajax({
+                    url: "http://127.0.0.1:3002/Asiakas/" + $(this).data("id"),
+                    type: "PUT",
+                    data: $(`form#edit-form`).serialize(),
+                    success: function(data){
+                        alert(data);
+                        editwindow.dialog( "close" );
+                        fetch(`form#edit-form`);
+                    },
+                    error: function(data){
+                        alert("Jotain epäonnistunut");
+                    }
+                })
+            },
+            Cancel: function() {
+                editwindow.dialog( "close" );
+            } 
+        }
+    })
+});
+
+showResultInTable = (result) => {
+    result.forEach(element => {
+        let trstr = "<tr id='elements'><td scope='row'>" + element.NIMI + "</td>";
+        trstr += "<td>" + element.OSOITE + "</td>";
+        trstr += "<td>" + element.POSTINRO + "</td>";
+        trstr += "<td>" + element.POSTITMP + "</td>";
+        trstr += "<td>" + element.LUONTIPVM + "</td>";
+        trstr += "<td>" + element.ASTY_AVAIN + "</td>";
+        // Tehtävä 6
+        trstr += "<td><input type='submit' class='btn btn-primary' value='Poista' onclick='poista(" + element.AVAIN + ")'></td>";
+        // Tehtävä 10
+        trstr += "<td><input type='button' class='btn btn-primary' value='Edit' onclick='edit(" + element.AVAIN + ")'></td>";
+        trstr += "</tr>";
+        $('#data tbody').append(trstr);
+        //$.get("http://127.0.0.1:3002/Types", function(data){tyyppi= String(Object.values(data)[element.ASTY_AVAIN-1].Selite)});
+    });
+}
